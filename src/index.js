3f247c4e477354764f96efd342e6a228905533c7
Berserker <nizamudeen5619@gmail.com>
2023-05-01 13:00:38 +0530

    added port availability check and error handling for starting server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import  app  from './app.js'
+import app from './app.js';
+import net from 'net';
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 
+// Check if the port is available
+const checkPortAvailability = (port) => {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.once('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        reject(`Port ${port} is already in use`);
+      } else {
+        reject(err);
+      }
+    });
+    server.once('listening', () => {
+      server.close();
+      resolve(`Port ${port} is available`);
+    });
+    server.listen(port);
+  });
+};
 
-app.listen(port, () => {
-    console.log('Server is running on port ', port)
-})
+// Start the server
+const startServer = async () => {
+  try {
+    await checkPortAvailability(port);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
