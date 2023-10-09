edbcdd79fd34492537b4be80e2c4e983770d9dc1
berserker5619 <Nizam@5619>
2023-10-09 19:49:21 +0530

    logging update

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.use((err, req, res, next) => {
 app.use((req, res, next) => {
 
     // Log request details
-    logger.info(`${req}`);
+    logger.info(`${req.toString()}`);
 
     // Log request headers
     logger.info('Request Headers:', req.headers);
