e16a1a89b9dd1d5b02b215702236322fb81fb335
berserker5619 <Nizam@5619>
2023-10-14 13:49:33 +0530

    logging req url

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.use((err, req, res, next) => {
 app.use((req, res, next) => {
 
     // Log request details
-    logger.info(`path:${req.path} method:${req.method}`);
+    logger.info(`url:${req.url} method:${req.method}`);
 
     // Log request headers
     logger.info('Request Headers:', req.headers);
